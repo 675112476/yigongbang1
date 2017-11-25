2320045cafa6e2220336fb320cf1a7388a1b6979
zhangxu <675112476@qq.com>
2017-11-25 16:54:55 +0800

    1125change5

--- app/src/main/java/com/example/sl/yigongbang/util/entity/Ip.java
+++ app/src/main/java/com/example/sl/yigongbang/util/entity/Ip.java
@@ -12,7 +12,8 @@ public class Ip {
 
     public static String getIp()  {
         //获得ip
-        String ip="http://106.14.9.53:8080/";
+        //String ip="http://106.14.9.53:8080/";
+        String ip ="http://192.168.2.143:8080/";
         return ip;
     }
 
