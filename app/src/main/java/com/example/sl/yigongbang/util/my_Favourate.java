369449b04a705945e3a99b2b3b613c2f63e51060
Zhang Xu <675112476@qq.com>
2017-12-12 22:12:23 +0800

    1.0版本

--- app/src/main/java/com/example/sl/yigongbang/util/my_Favourate.java
+++ app/src/main/java/com/example/sl/yigongbang/util/my_Favourate.java
@@ -2,6 +2,7 @@ package com.example.sl.yigongbang.util;
 
 import android.content.Intent;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
